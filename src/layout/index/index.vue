84d37cb7baf4b26578f97a81535989540e66ff6f
lichong <wslcwslc@126.com>
2024-01-29 17:22:16 +0800

    feat: 布局

--- src/layout/index/index.vue
+++ src/layout/index/index.vue
@@ -1,8 +1,23 @@
 <template>
-  布局
-  <RouterView />
+  <div class="flex layout-container">
+    <div class="login-header">
+      <LoginHeader />
+    </div>
+    <div class="router-container"><RouterView /></div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoginHeader from './components/loginHeader.vue';
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.layout-container {
+  height: 100%;
+  flex-direction: column;
+  .router-container {
+    height: 0;
+    flex-grow: 1;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './assets/style/reset.scss';
+import './assets/style/global.scss';
 import App from './App.vue';
 import router from '@/router';
 
